d9a5e2ec809e4415441bfe066817639785b8a04d
Juan-Estevan2117 <juanesvcano666@outlook.es>
2025-11-25 14:45:38 -0500

    making test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello CI/CD"}
+    assert response.json() == {"message": "Hello juanes"}
 
 
 def test_health_check():
